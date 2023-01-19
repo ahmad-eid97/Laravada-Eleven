948320c4fb8dbcd466cd2ce88006ca89f620e897
civil.ahmad.eid <civil.ahmad.eid@gmail.com>
2023-01-19 20:04:10 +0200

    add qoutaion page

--- pages/qoutation/qoutationHeading.vue
+++ pages/qoutation/qoutationHeading.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="heading">
+  <div class="page-heading">
     <div class="row justify-content-center m-0">
-      <h3 class="col-12 text-center title">Contact Us</h3>
+      <h3 class="col-12 text-center title">Post Qoutation</h3>
       <div class="col-12 text-center">
         <div class="breadcrumb">
           <router-link to="/"> Home </router-link>
           <a href="">
             <i class="fa-solid fa-angles-right"></i>
           </a>
-          <a href="#"> Contact Us </a>
+          <a href="/qoutation"> Qoutation </a>
         </div>
       </div>
     </div>
@@ -17,7 +17,7 @@
 
 <script>
 export default {
-  name: "AppContactHeading",
+  name: "AppBlogHeading",
 };
 </script>
 
